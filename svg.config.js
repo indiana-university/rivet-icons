364f25023a9818b18e4f7e186efc958d5a200d3c
lmcgrana <lmcgrana@iu.edu>
2019-05-03 09:53:19 -0400

    Added copyright to config file

--- svg.config.js
+++ svg.config.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (C) 2019 The Trustees of Indiana University
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 /**
  * Instead of a glob, it is possible to add paths to individual svg files
  * in the svgFilePaths Array below. Do this if you would rather create
