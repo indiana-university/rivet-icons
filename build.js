6d8bcca2a330eef1c6e71cbadd0880cb5e3fe99e
Chris Basham <chris@bash.am>
2021-04-20 12:30:52 -0400

    Convert all svg to CSS paths

--- build.js
+++ build.js
@@ -1,36 +1,41 @@
 const fs = require('fs')
+const { parse } = require('node-html-parser')
+const { pathThatSvg } = require('path-that-svg')
 
-const pathRE = new RegExp('<path.*d="(.+)".*/>', 'g')
-
-const files = fs.readdirSync('./src/svg')
-const icons = new Map()
 const namespace = 'rvt-icon'
 
-files.forEach((file) => {
-  const svg = fs.readFileSync(`./src/svg/${file}`, { encoding: 'utf8' })
-  const paths = [...svg.matchAll(pathRE)]
-    .filter((path) => path)
-    .map((path) => path[1])
-    .join(' ')
-  const name = file.replace('.svg', '')
-  icons.set(name, paths)
-})
+async function convert () {
+  const files = fs.readdirSync('./src/svg')
+  const icons = new Map()
 
-const iconVars = [...icons.entries()]
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i]
+    const source = fs.readFileSync(`./src/svg/${file}`, { encoding: 'utf8' })
+    const svg = await pathThatSvg(source)
+    const root = parse(svg)
+    const paths = root.querySelectorAll('path')
+      .map((el) => el.getAttribute('d'))
+      .map((d) => d.replace(/[\n\t]/g, ' '))
+      .join(' ')
+    const name = file.replace('.svg', '')
+    icons.set(name, paths)
+  }
+
+  const iconVars = [...icons.entries()]
   .map(([key, value]) => `  --${key}: path('${value}');`)
   .join('\n')
 
-const iconSelectors = [...icons.entries()]
-  .map(([key]) => [key, key.replace(`${namespace}-`, '')])
-  .map(([key, name]) =>
+  const iconSelectors = [...icons.entries()]
+    .map(([key]) => [key, key.replace(`${namespace}-`, '')])
+    .map(([key, name]) =>
 `.${namespace}--${name},
 [data-${namespace}="${name}"] {
   --${namespace}: var(--${key});
 }`)
-  .join('\n\n')
+    .join('\n\n')
 
-const css = `
-:root {
+  const css =
+`:root {
 ${iconVars}
 }
 
@@ -53,4 +58,7 @@ ${iconVars}
 ${iconSelectors}
 `
 
-fs.writeFileSync(`./dist/${namespace}s.css`, css)
+  fs.writeFileSync(`./dist/${namespace}s.css`, css)
+}
+
+convert()
